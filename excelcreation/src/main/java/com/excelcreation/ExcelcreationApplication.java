8d60981df5070c895de281f2dba2a9614fe30c58
sagnik15051998 <90502732+sagnik15051998@users.noreply.github.com>
2021-09-18 15:59:44 +0530

    Added initial fines

---
+++ excelcreation/src/main/java/com/excelcreation/ExcelcreationApplication.java
@@ -0,0 +1,13 @@
+package com.excelcreation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelcreationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelcreationApplication.class, args);
+	}
+
+}
---
+++ excelcreation/src/test/java/com/excelcreation/ExcelcreationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excelcreation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelcreationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
